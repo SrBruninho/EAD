4c122c201ce99c010f3167b8808a39cddc9cbcf1
Bruno Silva <bos.bruno.oliveira55@gmail.com>
2022-10-06 11:55:44 -0300

    Fluxo de deleção assíncrono

--- course/src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
+++ course/src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
@@ -47,6 +47,8 @@ public class CourseServiceImpl implements CourseService {
             });
            moduleRepository.deleteAll( moduleModelList );
         }
+
+        courseRepository.deleteCourseUserByCourse( courseModel.getCourseId() );
         courseRepository.delete( courseModel );
     }
 
