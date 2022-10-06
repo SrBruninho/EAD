4a10e0e08699febacc79555fdf7eabd11d51c8fd
Bruno Silva <bos.bruno.oliveira55@gmail.com>
2022-10-06 17:14:15 -0300

    Implementação do Circuit Breaker Pattern com resilience4J

--- authUser/src/main/java/com/ead/authUser/clients/CourseClient.java
+++ authUser/src/main/java/com/ead/authUser/clients/CourseClient.java
@@ -3,11 +3,14 @@ package com.ead.authUser.clients;
 import com.ead.authUser.dtos.CourseDTO;
 import com.ead.authUser.dtos.ResponsePageDTO;
 import com.ead.authUser.services.UtilsService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +18,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -31,6 +35,8 @@ public class CourseClient {
     @Value("${ead.api.url.course}")
     String REQUEST_URL_COURSE;
 
+    //@Retry(name = "retryInstance", fallbackMethod = "retryFallback")
+    @CircuitBreaker(name = "circuibreakerInstance")
     public Page<CourseDTO> getAllCoursesByUser(UUID userId, Pageable pageable){
         List<CourseDTO> searchResult = null;
         ResponseEntity<ResponsePageDTO<CourseDTO>> result = null;
@@ -49,5 +55,17 @@ public class CourseClient {
         log.info("Ending request /courses userId {}", userId);
 
         return result.getBody();
+    }
+    public Page<CourseDTO> circuitbreakerFallback( UUID userId, Pageable pageable, Throwable throwable){
+        log.error("Inside circuit breaker Fallback, cause {}", throwable);
+        List<CourseDTO> searchResult = new ArrayList<>();
+        return new PageImpl<>( searchResult );
+
+    }
+    public Page<CourseDTO> retryFallback( UUID userId, Pageable pageable, Throwable throwable){
+        log.error("Inside retry retryFallback, cause {}", throwable);
+        List<CourseDTO> searchResult = new ArrayList<>();
+        return new PageImpl<>( searchResult );
+
     }
 }
