b2277a0794cd1394e95e9eb5150ca84a3dfd94ba
Bruno Silva <bos.bruno.oliveira55@gmail.com>
2022-10-10 15:34:08 -0300

    Update authUser by refreshScope v4

--- authUser/src/main/java/com/ead/authUser/controllers/RefreshScopeController.java
+++ authUser/src/main/java/com/ead/authUser/controllers/RefreshScopeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 public class RefreshScopeController {
 
+    @Value("${authuser.refreshscope.name}")
     private String name;
 
     @RequestMapping("/refreshscope")
